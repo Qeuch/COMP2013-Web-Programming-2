d946436a561b7cca918346af2a083a35f5927ad3
Damien Sauve <damien.sauve4@gmail.com>
2025-09-17 09:55:45 -0400

    adding diamond world

---
+++ Tutorial-2/my-first-react-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import HelloWorld from './Components/HelloWorld';
+import Card from './Components/Card';
+
+function App() {
+
+  return (
+    <>
+      <h1>Diamond World</h1>
+      <div className="Main-Container">
+        <Card image="src\assets\images\1.jpg" name="princess" price="$1,350"/>
+        <Card image="src\assets\images\2.jpg" name="swan" price="$900"/>
+        <Card image="src\assets\images\3.jpg" name="collection" price="$1,100"/>
+      </div>
+    </>
+  )
+}
+
+export default App
